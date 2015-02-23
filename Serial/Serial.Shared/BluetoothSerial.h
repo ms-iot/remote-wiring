c807b4ab40dd688746b2b62c8db65902c19687bc
Zachary J. Fields <zachary_fields@yahoo.com>
2015-02-22 23:46:47 -0800

    Refactored Serial - added end() to ISerial and BluetoothSerial - moved Initialize() logic into begin() - added flag to control access to prevent crashing on early writes - added status functions and member to BluetoothSerial

--- Serial/Serial.Shared/BluetoothSerial.h
+++ Serial/Serial.Shared/BluetoothSerial.h
@@ -26,6 +26,12 @@ public:
     );
 
     virtual
+    void
+    end(
+        void
+    );
+
+    virtual
     uint16_t
     read (
         void
@@ -36,6 +42,26 @@ public:
     write (
         uint8_t c_
     );
+
+    bool
+    connectionReady(
+        void
+    );
+
+    Windows::Foundation::IAsyncOperation<Windows::Devices::Enumeration::DeviceInformationCollection ^> ^
+    listAvailableDevicesAsync(
+        void
+    );
+
+private:
+    Windows::Devices::Bluetooth::Rfcomm::RfcommDeviceService ^_device_service;
+    Windows::Storage::Streams::DataReader ^_rx;
+    Windows::Devices::Bluetooth::Rfcomm::RfcommServiceId ^_service_id;
+    Windows::Devices::Bluetooth::Rfcomm::RfcommServiceProvider ^_service_provider;
+    Windows::Networking::Sockets::StreamSocket ^_stream_socket;
+    Windows::Storage::Streams::DataWriter ^_tx;
+
+    LONG volatile _connection_ready;
 };
 
 }
