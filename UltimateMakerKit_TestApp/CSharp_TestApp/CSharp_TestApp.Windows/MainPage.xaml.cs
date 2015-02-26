b4a7af1c5ba9f0a246eb302af285025a141ff90e
Amitabh Nag <amnag@microsoft.com>
2015-02-26 10:57:19 -0800

    Changes made to the bluetooth app. Added 14 toggle buttons to turn LED ON/OFF, added 6 sliders for controlling the analog signal, added code to turn LED ON/OFF when the toggle buttons are toggled. Functionality to modify analog signal thru sliders is not yet implemented.

--- UltimateMakerKit_TestApp/CSharp_TestApp/CSharp_TestApp.Windows/MainPage.xaml.cs
+++ UltimateMakerKit_TestApp/CSharp_TestApp/CSharp_TestApp.Windows/MainPage.xaml.cs
@@ -52,10 +52,289 @@ namespace CSharp_TestApp
             _bt_serial.end();
             OutputText("Disconnected.");
         }
-
-        private void Clicked_OffButton(object sender, RoutedEventArgs e)
+        
+        private void Pin0_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
         {
-            _arduino_uno.digitalWrite(13, 0);
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(0, 1);
+                    OutputText("LED 0 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(0, 0);
+                    OutputText("LED 0 off");
+                }
+            }
+        }
+
+        private void Pin1_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(1, 1);
+                    OutputText("LED 1 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(1, 0);
+                    OutputText("LED 1 off");
+                }
+            }
+        }
+
+        private void Pin2_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(2, 1);
+                    OutputText("LED 2 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(2, 0);
+                    OutputText("LED 2 off");
+                }
+            }
+        }
+        private void Pin3_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(3, 1);
+                    OutputText("LED 3 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(3, 0);
+                    OutputText("LED 3 off");
+                }
+            }
+        }
+        private void Pin4_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(4, 1);
+                    OutputText("LED 4 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(4, 0);
+                    OutputText("LED 4 off");
+                }
+            }
+        }
+
+        private void Pin5_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(5, 1);
+                    OutputText("LED 5 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(5, 0);
+                    OutputText("LED 5 off");
+                }
+            }
+        }
+
+        private void Pin6_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(6, 1);
+                    OutputText("LED 6 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(6, 0);
+                    OutputText("LED 6 off");
+                }
+            }
+        }
+
+        private void Pin7_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(7, 1);
+                    OutputText("LED 7 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(7, 0);
+                    OutputText("LED 7 off");
+                }
+            }
+        }
+
+        private void Pin8_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(8, 1);
+                    OutputText("LED 8 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(8, 0);
+                    OutputText("LED 8 off");
+                }
+            }
+        }
+
+        private void Pin9_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(9, 1);
+                    OutputText("LED 9 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(9, 0);
+                    OutputText("LED 9 off");
+                }
+            }
+        }
+
+        private void Pin10_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(10, 1);
+                    OutputText("LED 10 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(10, 0);
+                    OutputText("LED 10 off");
+                }
+            }
+        }
+
+        private void Pin11_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(11, 1);
+                    OutputText("LED 11 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(11, 0);
+                    OutputText("LED 11 off");
+                }
+            }
+        }
+
+        private void Pin12_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(12, 1);
+                    OutputText("LED 12 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(12, 0);
+                    OutputText("LED 12 off");
+                }
+            }
+        }
+
+        private void Pin13_ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    _arduino_uno.digitalWrite(13, 1);
+                    OutputText("LED 13 on");
+                }
+                else
+                {
+                    _arduino_uno.digitalWrite(13, 0);
+                    OutputText("LED 13 off");
+                }
+            }
+        }
+
+        private void Pin3_Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+        }
+
+        private void Pin5_Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+        }
+
+        private void Pin6_Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+        }
+
+        private void Pin9_Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+        }
+
+        private void Pin10_Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+        }
+        private void Pin11_Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+        }
+
+        // Remove code below this line
+        private void TextBlock_SelectionChanged_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clicked_OffButton_OLD(object sender, RoutedEventArgs e)
+        {
+            /*AN _arduino_uno.digitalWrite(13, 0);
             _arduino_uno.digitalWrite(12, 0);
             _arduino_uno.digitalWrite(11, 0);
             _arduino_uno.digitalWrite(10, 0);
@@ -68,13 +347,13 @@ namespace CSharp_TestApp
             _arduino_uno.digitalWrite(3, 0);
             _arduino_uno.digitalWrite(2, 0);
             //_arduino_uno.digitalWrite(1, 0);  // TX
-            //_arduino_uno.digitalWrite(0, 0);  // RX
+            //_arduino_uno.digitalWrite(0, 0);  // RX*/
             OutputText("LED off.");
         }
 
-        private void Clicked_OnButton(object sender, RoutedEventArgs e)
+        private void Clicked_OnButton_OLD(object sender, RoutedEventArgs e)
         {
-            _arduino_uno.digitalWrite(13, 1);
+            /*AN_arduino_uno.digitalWrite(13, 1);
             _arduino_uno.digitalWrite(12, 1);
             _arduino_uno.digitalWrite(11, 1);
             _arduino_uno.digitalWrite(10, 1);
@@ -87,13 +366,35 @@ namespace CSharp_TestApp
             _arduino_uno.digitalWrite(3, 1);
             _arduino_uno.digitalWrite(2, 1);
             //_arduino_uno.digitalWrite(1, 1);  // TX
-            //_arduino_uno.digitalWrite(0, 1);  // RX
+            //_arduino_uno.digitalWrite(0, 1);  // RX */
             OutputText("LED on.");
         }
 
-        private void OutputText(String textString) {
+        private void OutputText(String textString)
+        {
             OutputBlock.SelectAll();
-            OutputBlock.Text = OutputBlock.SelectedText + textString;
+            OutputBlock.Text = OutputBlock.SelectedText + "Status: " + textString;
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+        private void OutputBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
