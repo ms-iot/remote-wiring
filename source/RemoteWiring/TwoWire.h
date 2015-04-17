507262c1b6a0e91fd990c05f89faf7886337991f
Jesse Frush <jesse.frush@gmail.com>
2015-04-17 01:31:53 -0700

    Added new events to the Wiring layer for digital/analog pin updates and I2C replies.

--- source/RemoteWiring/TwoWire.h
+++ source/RemoteWiring/TwoWire.h
@@ -8,12 +8,14 @@ ref class RemoteDevice;
 
 namespace I2c {
 
-public delegate void I2cCallback( uint8_t address_, Platform::String ^ response );
+public delegate void I2cReplyCallback( uint8_t address_, Platform::String ^ response );
 
 public ref class TwoWire sealed
 {
 public:
 	friend ref class RemoteDevice;
+
+	event I2cReplyCallback ^ I2cReplyEvent;
 
 	//enables I2C with a 10ms delay time for requesting a response from the slave device
 	void
@@ -43,11 +45,10 @@ public:
 	void
 	read(
 		uint8_t address_,
-		size_t numBytes_,
-		I2cCallback ^callback_
+		size_t numBytes_
 		) 
 	{
-		read( address_, numBytes_, 0xFF, false, callback_ );
+		read( address_, numBytes_, 0xFF, false );
 	}
 
 	//a one-time read which will prompt the given register for data
@@ -56,11 +57,10 @@ public:
 	read(
 		uint8_t address_,
 		size_t numBytes_,
-		uint8_t reg_,
-		I2cCallback ^callback_
+		uint8_t reg_
 		)
 	{
-		read( address_, numBytes_, reg_, false, callback_ );
+		read( address_, numBytes_, reg_, false );
 	}
 
 	/*
@@ -72,8 +72,7 @@ public:
 		uint8_t address_,
 		size_t numBytes_,
 		uint8_t reg_,
-		bool continuous_,
-		I2cCallback ^callback_
+		bool continuous_
 	);
 
 	void
@@ -86,19 +85,15 @@ private:
 	TwoWire(
 		Firmata::UAPFirmataClient ^ firmata_
 		) :
-		_firmata( firmata_ ),
-		_i2cReplyCallback( nullptr )
+		_firmata( firmata_ )
 	{
-		_i2cReplyCallback = nullptr;
-		_firmata->I2cEvent += ref new Firmata::I2cReplyCallbackFunction( [this]( Firmata::UAPFirmataClient ^caller, Firmata::I2cCallbackEventArgs^ args ) -> void { onI2cReply( args ); } );
+		_firmata->I2cReplyEvent += ref new Firmata::I2cReplyCallbackFunction( [this]( Firmata::UAPFirmataClient ^caller, Firmata::I2cCallbackEventArgs^ args ) -> void { onI2cReply( args ); } );
 	}
 
 	//since 16 bit values are sent as two 7 bit bytes, you can't send a value larger than this across the wire
 	const uint16_t MAX_READ_DELAY_MICROS = 0x3FFF;
 
 	void onI2cReply( Firmata::I2cCallbackEventArgs ^argv );
-
-	I2cCallback ^_i2cReplyCallback;
 
 	//a reference to the UAP firmata interface
 	Firmata::UAPFirmataClient ^_firmata;
