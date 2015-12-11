ebe7e9400b26c6cd8ab5983ac98fae7a012ceb39
HerrickSpencerMSFT <IOTMe@HerrickSpencer.com>
2015-12-11 14:56:43 -0800

    Adding additional tests Test_connection_USB Test_ConnectionStatusEvent Test_TotalPinCount Test_AnalogPinCount Test_GPIOPinModeSetGet  Test_AnalogPinModeSetGet

---
+++ Microsoft.Maker.win10/RemoteWiringUnitTests/RemoteDeviceHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Maker.Serial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+
+namespace RemoteWiringUnitTests
+{
+    /// <summary>
+    /// Helper class for gathering device information or connecting to devices
+    /// </summary>
+    public static class RemoteDeviceHelper
+    {
+        /// <summary>
+        /// Get arduino devices connected.
+        /// </summary>
+        /// <remarks>Requires arduino drivers installed for name resolution</remarks>
+        /// <returns>last arduino device found</returns>
+        public static DeviceInformation GetDevice()
+        {
+            DeviceInformation returnDevice = null;
+            Task<DeviceInformationCollection> task = GetDeviceListAsync();
+            var result = task.Result;
+            foreach (DeviceInformation device in result)
+            {
+                if (device.Name.Contains("Arduino"))
+                    returnDevice = device;
+            }
+
+            return returnDevice;
+        }
+
+        /// <summary>
+        /// Gather list of all devices (including non-arduino) attached via USB
+        /// </summary>
+        /// <returns>DeviceInformationCollection of devices</returns>
+        public static Task<DeviceInformationCollection> GetDeviceListAsync()
+        {
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource(30000);
+            Task<DeviceInformationCollection> task = UsbSerial.listAvailableDevicesAsync().AsTask<DeviceInformationCollection>(cancelTokenSource.Token);
+            return task;
+        }
+    }
+
+    internal static class ExpectedDeviceHardware
+    {
+
+    }
+}
--- Microsoft.Maker.win10/RemoteWiringUnitTests/UnitTest.cs
+++ Microsoft.Maker.win10/RemoteWiringUnitTests/UnitTest.cs
@@ -1,89 +1,216 @@
-﻿using System;
-using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
-using Microsoft.Maker.RemoteWiring;
+﻿using Microsoft.Maker.RemoteWiring;
 using Microsoft.Maker.Serial;
-using Windows.Devices.Enumeration;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
 using System.Threading;
+using Windows.Devices.Enumeration;
 
 namespace RemoteWiringUnitTests
 {
     [TestClass]
     public class UnitTest1
     {
+        private const int TIMEOUT = 30000;
         private uint baudRate = 9600;
         private string connectionStatus;
+        private RemoteDevice arduinoDevice;
+        private static IStream connection;
+        private Dictionary<byte, PinState> pinStates = new Dictionary<byte, PinState>();
+        private Dictionary<byte, ushort> analogPinValues = new Dictionary<byte, ushort>();
 
         [TestInitialize]
         public void InitTest()
         {
-            this.connectionStatus = string.Empty;
+            connectionStatus = string.Empty;
+            if (null != connection && connection.connectionReady())
+            {
+                connection.end();
+                connectionStatus = null;
+            }
         }
-        
+
+        #region Tests
         [TestMethod]
-        public void ConnectionTest_USB()
+        public void Test_Connection_USB()
+        {
+            // asserts are in helper function
+            ConnectDevice();
+        }
+
+        [TestMethod]
+        public void Test_Connection_statusEvent()
+        {
+            ConnectDevice();
+
+            DateTime timeout = DateTime.UtcNow.AddSeconds(30);
+            System.Threading.SpinWait.SpinUntil(() => (
+                timeout <= DateTime.UtcNow ||
+                !String.IsNullOrEmpty(connectionStatus)
+                ));
+
+            Assert.AreEqual("Ready", connectionStatus, "OnDeviceReady event was not fired");
+        }
+
+        [TestMethod]
+        public void Test_TotalPinCount()
+        {
+            // todo: create classes to dynamically set expected values based on board type.
+            int expectedPinCount = 30;
+            HardwareProfile hardware = GetHardwareProfile();
+            Assert.AreEqual(expectedPinCount, hardware.TotalPinCount, "Pin Count on device incorrect");
+        }
+
+        [TestMethod]
+        public void Test_AnalogPinCount()
+        {
+            // todo: create classes to dynamically set expected values based on board type.
+            int expectedPinCount = 12;
+            HardwareProfile hardware = GetHardwareProfile();
+            Assert.AreEqual(expectedPinCount, hardware.AnalogPinCount, "Pin Count on device incorrect");
+        }
+
+        [TestMethod]
+        public void Test_GPIOPinCount()
+        {
+            Assert.Fail("GPIOPinCount Not supported yet");
+            //// todo: create classes to dynamically set expected values based on board type.
+            //int expectedPinCount = 12;
+            //HardwareProfile hardware = GetHardwareProfile();
+            //Assert.AreEqual(expectedPinCount, hardware.AnalogPinCount, "Pin Count on device incorrect");
+        }
+
+        /// <summary>
+        /// Tests output and input of digital pins
+        /// </summary>
+        /// <remarks>Requires a jumper between pins 3 & 4 </remarks>
+        [TestMethod]
+        public void Test_GPIOPinModeSetGet()
+        {
+            byte pinOut = 3;
+            byte pinIn = 4;
+            PinMode pinInMode = PinMode.INPUT;
+            PinMode pinOutMode = PinMode.OUTPUT;
+            HardwareProfile hardware = GetHardwareProfile();
+
+            PinMode mode = arduinoDevice.getPinMode(pinIn);
+            if (mode != pinInMode)
+            {
+                arduinoDevice.pinMode(pinIn, pinInMode);
+            }
+            mode = arduinoDevice.getPinMode(pinOut);
+            if (mode != pinOutMode)
+            {
+                arduinoDevice.pinMode(pinOut, pinOutMode);
+            }
+
+            arduinoDevice.digitalWrite(pinOut, PinState.HIGH);
+            arduinoDevice.DigitalPinUpdated += ArduinoDevice_DigitalPinUpdated;
+
+            SpinWait.SpinUntil(() => (pinStates.ContainsKey(pinIn)), TIMEOUT);
+
+            PinState state = arduinoDevice.digitalRead(pinIn);
+            Assert.AreEqual(PinState.HIGH, state);
+        }
+
+        /// <summary>
+        /// Tests output and input of digital pins
+        /// </summary>
+        /// <remarks>Requires a jumper between pins 5 & 14 (A0) </remarks>
+        [TestMethod]
+        public void Test_AnalogPinModeSetGet()
+        {
+            HardwareProfile hardware = GetHardwareProfile();
+
+            byte pinOut = 5;
+            string pinInAnalog = "A0";
+            PinMode pinInMode = PinMode.ANALOG;
+            PinMode pinOutMode = PinMode.OUTPUT;
+            ushort expectedValue = 1023;
+
+            PinMode mode = arduinoDevice.getPinMode(pinInAnalog);
+            if (mode != pinInMode)
+            {
+                arduinoDevice.pinMode(pinInAnalog, pinInMode);
+            }
+            mode = arduinoDevice.getPinMode(pinOut);
+            if (mode != pinOutMode)
+            {
+                arduinoDevice.pinMode(pinOut, pinOutMode);
+            }
+
+            arduinoDevice.digitalWrite(pinOut, PinState.HIGH);
+            arduinoDevice.AnalogPinUpdated += ArduinoDevice_AnalogPinUpdated;
+
+            SpinWait.SpinUntil(() => (analogPinValues.ContainsKey(14)), TIMEOUT);
+
+            ushort state = arduinoDevice.analogRead(pinInAnalog);
+            Assert.IsTrue( expectedValue - state <= 100, "Analog Value {0} is not within 100 of expected value {1}", state, expectedValue);
+        }
+        #endregion tests
+
+        #region helpers
+        private void ConnectDevice()
         {
             // Get List of devices and select Arduino
             DeviceInformation device = null;
-            device = GetDevice();
+            device = RemoteDeviceHelper.GetDevice();
             Assert.IsNotNull(device);
 
             // setup and start connection events
-            IStream connection = new UsbSerial(device);
-            RemoteDevice arduinoDevice = new RemoteDevice(connection);
+            connection = new UsbSerial(device);
+            arduinoDevice = new RemoteDevice(connection);
             arduinoDevice.DeviceReady += OnDeviceReady;
             arduinoDevice.DeviceConnectionFailed += OnConnectionFailed;
             connection.begin(baudRate, SerialConfig.SERIAL_8N1);
 
             // Wait for connection
-            DateTime timeout = DateTime.UtcNow.AddSeconds(30);
+            DateTime timeout = DateTime.UtcNow.AddMilliseconds(TIMEOUT);
             System.Threading.SpinWait.SpinUntil(() => (
                 timeout <= DateTime.UtcNow ||
                 connection.connectionReady() ||
-                !String.IsNullOrEmpty(this.connectionStatus)
+                !String.IsNullOrEmpty(connectionStatus)
                 ));
 
             // Test if connected properly
-            Assert.IsTrue(connection.connectionReady(), this.connectionStatus);
-
-            timeout = DateTime.UtcNow.AddSeconds(30);
-            System.Threading.SpinWait.SpinUntil(() => (
-                timeout <= DateTime.UtcNow ||
-                !String.IsNullOrEmpty(this.connectionStatus)
-                ));
-
-            Assert.AreEqual(this.connectionStatus, "Ready", "OnDeviceReady event was not fired");
+            Assert.IsTrue(connection.connectionReady(), connectionStatus);
         }
 
-        private static DeviceInformation GetDevice()
+        private void ConnectDeviceWaitOnEvent()
         {
-            DeviceInformation returnDevice = null;
-            Task<DeviceInformationCollection> task = GetDeviceAsync();
-            var result = task.Result;
-            foreach (DeviceInformation device in result)
-            {
-                if (device.Name.Contains("Arduino"))
-                    returnDevice = device;
-            }
-
-            return returnDevice;
+            ConnectDevice();
+            SpinWait.SpinUntil(() => { return !string.IsNullOrEmpty(connectionStatus); }, TIMEOUT);
         }
 
-        private static Task<DeviceInformationCollection> GetDeviceAsync()
+        private HardwareProfile GetHardwareProfile()
         {
-            CancellationTokenSource cancelTokenSource = new CancellationTokenSource(30000);
-            Task<DeviceInformationCollection> task = UsbSerial.listAvailableDevicesAsync().AsTask<DeviceInformationCollection>(cancelTokenSource.Token);
-            return task;
+            ConnectDeviceWaitOnEvent();
+            return arduinoDevice.DeviceHardwareProfile;
         }
+        #endregion helpers
 
+        #region connection event handlers
         private void OnConnectionFailed(string message)
         {
-            this.connectionStatus = "Failed: " + message;
+            connectionStatus = "Failed: " + message;
         }
 
         private void OnDeviceReady()
         {
-            this.connectionStatus = "Ready";
+            connectionStatus = "Ready";
         }
+
+        private void ArduinoDevice_DigitalPinUpdated(byte pin, PinState state)
+        {
+            this.pinStates.Remove(pin);
+            this.pinStates.Add(pin, state);
+        }
+
+        private void ArduinoDevice_AnalogPinUpdated(byte pin, ushort value)
+        {
+            this.analogPinValues.Remove(pin);
+            this.analogPinValues.Add(pin, value);
+        }
+        #endregion event handlers
     }
 }
